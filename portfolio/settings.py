de8cac9a0a3aa2157f4b463ff967678920e2fd01
meet0625 <143720992+meet0625@users.noreply.github.com>
2026-01-01 11:20:08 +0530

    Fix Koyeb build failure

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -2,8 +2,8 @@
 from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'comic-theme-toggle'
-DEBUG = True
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ["*"]
 INSTALLED_APPS = [
  'django.contrib.admin','django.contrib.auth','django.contrib.contenttypes',
  'django.contrib.sessions','django.contrib.messages','django.contrib.staticfiles',
@@ -16,6 +16,7 @@ MIDDLEWARE = [
  'django.middleware.csrf.CsrfViewMiddleware',
  'django.contrib.auth.middleware.AuthenticationMiddleware',
  'django.contrib.messages.middleware.MessageMiddleware',
+ 'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [{
@@ -32,4 +33,5 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 DATABASES = {'default':{'ENGINE':'django.db.backends.sqlite3','NAME':BASE_DIR/'db.sqlite3'}}
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR/'main'/'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
